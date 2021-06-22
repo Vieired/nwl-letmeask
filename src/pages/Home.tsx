0f5becfd0ef2898fcc1b925c5d863758f14ed88b
egomes <egomes@metatron.com>
2021-06-21 23:33:48 -0300

    fix: corrige importação do firebase na Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from 'react-router-dom';
-import { auth, firebase } from '../services/services';
+import { auth, firebase } from './services/firebase';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
