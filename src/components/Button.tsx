a7ddcc0f9ac0cae39f5d107953c48f01e1d6c559
egomes <egomes@metatron.com>
2021-06-22 20:12:08 -0300

    doc: documenta o componente Button com comentários diretos

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,10 @@
 import { ButtonHTMLAttributes } from 'react';
 import '../styles/button.scss';
 
+/* 
+ * Interface ButtonProps
+ * A tipagem ButtonHTMLAttributes permite ao typescript saber o que exibir no Intelicense no Button
+*/
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
