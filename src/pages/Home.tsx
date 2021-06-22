c93ac37207bd62908d7a7cf0d7789226b6984dd1
egomes <egomes@metatron.com>
2021-06-22 19:18:19 -0300

    feat: implementa recuperação do estado de autenticação do usuário e refatora

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,14 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import { auth, firebase } from '../services/firebase';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 import '../styles/auth.scss';
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext);
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
         if(!user) {
