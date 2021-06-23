5f85521e4993d20e0ee170a4d7e70c72a997f859
egomes <egomes@metatron.com>
2021-06-22 22:12:38 -0300

    fix: corrige operador de igualdade

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -20,7 +20,7 @@ export function Room() {
     async function handleSendQuestion(event: FormEvent) {
         event.preventDefault();
 
-        if(newQuestion.trim() == '') {
+        if(newQuestion.trim() === '') {
             return;
         }
 
